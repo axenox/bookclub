2869cfdf7bd13af9dc70cbe7e59734a634c3e006
aka <aka@NBDR223.salt-solutions.de>
2020-09-11 19:26:43 +0200

    FIX SQL installer

--- BookClubApp.php
+++ BookClubApp.php
@@ -3,7 +3,6 @@ namespace axenox\BookClub;
 
 use exface\Core\Interfaces\InstallerInterface;
 use exface\Core\CommonLogic\Model\App;
-use exface\Core\Exceptions\Model\MetaObjectNotFoundError;
 use exface\Core\CommonLogic\AppInstallers\MySqlDatabaseInstaller;
 
 class BookClubApp extends App
@@ -13,14 +12,10 @@ class BookClubApp extends App
         $installer = parent::getInstaller($injected_installer);
         
         $sqlInstaller = new MySqlDatabaseInstaller($this->getSelector());        
-        try {
-            $sqlInstaller
-            ->setFoldersWithMigrations(['InitDB','Migrations'])
-            ->setDataSourceSelector('0x11eab5facf6370bab5fa0205857feb80');
-            $installer->addInstaller($sqlInstaller);
-        } catch (MetaObjectNotFoundError $e) {
-            $this->getWorkbench()->getLogger()->warning('Cannot init SqlSchemInstaller for app ' . $this->getAliasWithNamespace() . ': no model there yet!');
-        }
+        $sqlInstaller
+        ->setFoldersWithMigrations(['InitDB','Migrations'])
+        ->setDataSourceSelector('0x11eab5facf6370bab5fa0205857feb80');
+        $installer->addInstaller($sqlInstaller);
         
         return $installer;
     }
