35e9e372112df3cb823ecc5bb78045ddb55d3583
aka <aka@NBDR223.salt-solutions.de>
2020-10-15 11:21:30 +0200

    FIX do_not_loan instead of available_for_loan

---
+++ Install/Sql/MySQL/Migrations/1.0/20201015_1104_FIX_flag_available_for_loan_renamed.sql
@@ -0,0 +1,19 @@
+-- UP
+	
+ALTER TABLE `book`
+	ADD COLUMN `do_not_loan` INT(1) NOT NULL DEFAULT '0' AFTER `owner_rating`;
+	
+UPDATE `book` SET `do_not_load` = IF(available_for_loan = 1, 0, 1);
+
+ALTER TABLE `book`
+	DROP COLUMN `available_for_loan`;
+	
+-- DOWN
+
+ALTER TABLE `book`
+	ADD COLUMN `available_for_loan` INT(1) NOT NULL DEFAULT '0' AFTER `owner_rating`;
+	
+UPDATE `book` SET `available_for_loan` = IF(do_not_loan = 1, 0, 1);
+
+ALTER TABLE `book`
+	DROP COLUMN `do_not_loan`;
