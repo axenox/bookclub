dfb53486405d07317bda61830f31b6afc6388ca4
aka <aka@NBDR223.salt-solutions.de>
2020-10-15 11:26:38 +0200

    FIX typo in SQL migration

--- Install/Sql/MySQL/Migrations/1.0/20201015_1104_FIX_flag_available_for_loan_renamed.sql
+++ Install/Sql/MySQL/Migrations/1.0/20201015_1104_FIX_flag_available_for_loan_renamed.sql
@@ -3,7 +3,7 @@
 ALTER TABLE `book`
 	ADD COLUMN `do_not_loan` INT(1) NOT NULL DEFAULT '0' AFTER `owner_rating`;
 	
-UPDATE `book` SET `do_not_load` = IF(available_for_loan = 1, 0, 1);
+UPDATE `book` SET `do_not_loan` = IF(available_for_loan = 1, 0, 1);
 
 ALTER TABLE `book`
 	DROP COLUMN `available_for_loan`;
